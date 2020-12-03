9e305da143876b31f7b368365141623b7c1d0e33
Glitch (lateral-wistful-hour) <none>
2020-12-03 23:25:17 +0000

    🏨🏑 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+let year;
+let age;
+let name;
+$(".calculate").click(function(){
+  year= Number($ (".year").val())
+  name=$(".name").val;
+  console.log (name)
+  console.log(year)
+});
+let currentYear=new Date ().getFullYear();
+age= currentYear - year;
+$(".message").text(${name}), you seem to be ${age} years old.);
